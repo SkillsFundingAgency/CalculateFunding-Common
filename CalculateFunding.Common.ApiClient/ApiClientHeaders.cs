8bbb6e544822d317baaeb412b005e1bef7815d62
Dan Symonds-Lloyd <Daniel.SYMONDS-LLOYD@education.gov.uk>
2018-11-08 13:48:24 +0000

    Merged PR 621: API Keys class rename to fix spelling

---
+++ CalculateFunding.Common.ApiClient/ApiClientHeaders.cs
@@ -0,0 +1,10 @@
+﻿namespace CalculateFunding.Common.ApiClient
+{
+    public static class ApiClientHeaders
+    {
+        public const string ApiKey = "Ocp-Apim-Subscription-Key";
+        public const string CorrelationId = "sfa-correlationId";
+        public const string Username = "sfa-username";
+        public const string UserId = "sfa-userid";
+    }
+}
