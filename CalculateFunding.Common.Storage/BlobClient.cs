b1ea22f02c02d8704098cfa071c2a64be4d75e0f
David Gent <David.GENT@education.gov.uk>
2019-02-14 09:20:26 +0000

    Merged PR 1885: Common blob client
    Common blob client
    
    Related work items: #8443

---
+++ CalculateFunding.Common.Storage/BlobClient.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using CalculateFunding.Common.Utility;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Newtonsoft.Json;
+
+namespace CalculateFunding.Common.Storage
+{
+    public abstract class BlobClient
+    {
+        private Lazy<CloudBlobContainer> _container;
+        private readonly BlobStorageOptions _azureStorageSettings;
+
+        public BlobClient(BlobStorageOptions blobStorageSettings)
+        {
+            Guard.ArgumentNotNull(blobStorageSettings, nameof(blobStorageSettings));
+            Guard.IsNullOrWhiteSpace(blobStorageSettings.ConnectionString, nameof(blobStorageSettings.ConnectionString));
+            Guard.IsNullOrWhiteSpace(blobStorageSettings.ContainerName, nameof(blobStorageSettings.ContainerName));
+
+            _azureStorageSettings = blobStorageSettings;
+
+            EnsureBlobClient();
+        }
+
+        public static void VerifyFileName(string fileName)
+        {
+            Guard.IsNullOrWhiteSpace(fileName, nameof(fileName));
+
+            if (fileName.Length > 254)
+            {
+                throw new ArgumentException("Storage File name is too long, max length is 254 characters", nameof(fileName));
+            }
+
+            if (fileName.EndsWith(".", StringComparison.OrdinalIgnoreCase))
+            {
+                throw new ArgumentException("Storage File name cannot end with a period (.)", nameof(fileName));
+            }
+
+            if (fileName.EndsWith("/", StringComparison.OrdinalIgnoreCase))
+            {
+                throw new ArgumentException("Storage File name cannot end with a forward slash (/)", nameof(fileName));
+            }
+        }
+
+        public async Task<(bool Ok, string Message)> IsHealthOk()
+        {
+            try
+            {
+                EnsureBlobClient();
+                CloudBlobContainer container = _container.Value;
+                return await Task.FromResult((true, string.Empty));
+            }
+            catch (Exception ex)
+            {
+                return (false, ex.Message);
+            }
+        }
+
+        public async Task<bool> DoesBlobExistAsync(string blobName)
+        {
+            Guard.IsNullOrWhiteSpace(blobName, nameof(blobName));
+
+            EnsureBlobClient();
+
+            ICloudBlob blob = await _container.Value.GetBlobReferenceFromServerAsync(blobName);
+
+            return await blob.ExistsAsync();
+        }
+
+        public string GetBlobSasUrl(string blobName, DateTimeOffset finish, SharedAccessBlobPermissions permissions)
+        {
+            ICloudBlob blob = GetBlockBlobReference(blobName);
+            
+            SharedAccessBlobPolicy sasConstraints = new SharedAccessBlobPolicy
+            {
+                SharedAccessExpiryTime = finish,
+                Permissions = permissions
+            };
+
+            string sharedAccessSignature = blob.GetSharedAccessSignature(sasConstraints);
+
+            return $"{blob.Uri}{sharedAccessSignature}";
+        }
+
+        public async Task<T> DownloadAsync<T>(string blobName)
+        {
+            Guard.IsNullOrWhiteSpace(blobName, nameof(blobName));
+
+            EnsureBlobClient();
+
+            CloudBlockBlob blob = _container.Value.GetBlockBlobReference(blobName);
+            string json = await blob.DownloadTextAsync();
+
+            return JsonConvert.DeserializeObject<T>(json);
+        }
+
+        public async Task<string> UploadFileAsync<T>(string blobName, T contents)
+        {
+            Guard.ArgumentNotNull(contents, nameof(contents));
+
+            string json = JsonConvert.SerializeObject(contents);
+
+            return await UploadFileAsync(blobName, json);
+        }
+
+        protected ICloudBlob GetBlockBlobReference(string blobName)
+        {
+            Guard.IsNullOrWhiteSpace(blobName, nameof(blobName));
+
+            EnsureBlobClient();
+            
+            return _container.Value.GetBlockBlobReference(blobName);
+        }
+
+        protected Task<ICloudBlob> GetBlobReferenceFromServerAsync(string blobName)
+        {
+            Guard.IsNullOrWhiteSpace(blobName, nameof(blobName));
+
+            EnsureBlobClient();
+
+            return _container.Value.GetBlobReferenceFromServerAsync(blobName);
+        }
+
+        protected async Task<Stream> DownloadToStreamAsync(ICloudBlob blob)
+        {
+            Guard.ArgumentNotNull(blob, nameof(blob));
+
+            EnsureBlobClient();
+
+            MemoryStream stream = new MemoryStream();
+            await blob.DownloadToStreamAsync(stream);
+            stream.Position = 0;
+
+            return stream;
+        }
+
+        protected async Task<string> UploadFileAsync(string blobName, string fileContents)
+        {
+            Guard.IsNullOrWhiteSpace(blobName, nameof(blobName));
+
+            VerifyFileName(blobName);
+
+            EnsureBlobClient();
+
+            CloudBlockBlob blob = _container.Value.GetBlockBlobReference(blobName);
+
+            await blob.UploadTextAsync(fileContents);
+
+            return blob.Uri.ToString();
+        }
+
+        private void EnsureBlobClient()
+        {
+            if (_container == null)
+            {
+                _container = new Lazy<CloudBlobContainer>(() =>
+                {
+                    CloudStorageAccount credentials = CloudStorageAccount.Parse(_azureStorageSettings.ConnectionString);
+                    CloudBlobClient client = credentials.CreateCloudBlobClient();
+                    CloudBlobContainer container = client.GetContainerReference(_azureStorageSettings.ContainerName.ToLower());
+
+                    container.CreateIfNotExists();
+
+                    return container;
+                });
+            }
+        }
+    }
+}
---
+++ CalculateFunding.Common.Storage/BlobStorageOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace CalculateFunding.Common.Storage
+{
+    public class BlobStorageOptions
+    {
+        public string ConnectionString { get; set; }
+
+        public string ContainerName { get; set; }
+    }
+}
