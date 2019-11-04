4bcfe12aa4b6e696291874abeb02db0a45b6574c
Antony GORMAN <Antony.GORMAN@education.gov.uk>
2019-11-04 16:08:49 +0000

    Merged PR 5305: FIX for weird changes to calc current version model
    FIX for weird changes to calc current version model
    
    Related work items: #30729

--- CalculateFunding.Common.ApiClient.Calcs/Models/CalculationCurrentVersion.cs
+++ CalculateFunding.Common.ApiClient.Calcs/Models/CalculationCurrentVersion.cs
@@ -1,9 +1,29 @@
-﻿namespace CalculateFunding.Common.ApiClient.Calcs.Models
-{
-    public class CalculationCurrentVersion : Calculation
-    {
-        public string PolicyId { get; set; }
+﻿using System;
+using CalculateFunding.Common.ApiClient.Models;
+using CalculateFunding.Common.Models;
 
-        public string PolicyName { get; set; }
+namespace CalculateFunding.Common.ApiClient.Calcs.Models
+{
+    public class CalculationCurrentVersion : Reference
+    {
+        public string SpecificationId { get; set; }
+
+        public string FundingPeriodName { get; set; }
+
+        public string FundingStreamId { get; set; }
+
+        public string SourceCode { get; set; }
+
+        public DateTimeOffset? Date { get; set; }
+
+        public Reference Author { get; set; }
+
+        public int Version { get; set; }
+
+        public string CalculationType { get; set; }
+
+        public PublishStatus PublishStatus { get; set; }
+
+        public string SourceCodeName { get; set; }
     }
 }
