ab3f4f0882baab6d87583b1d1bff95370bf3f2fc
Rohit AMBLEKAR <Rohit.AMBLEKAR@education.gov.uk>
2020-01-03 09:38:42 +0000

    Merged PR 5746: Model Unit Test project added
    Model Unit Test project added
    
    Related work items: #33745

---
+++ CalculateFunding.Common.Models.UnitTests/ExtensionModelTests.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using CalculateFunding.Common.Models;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CalculateFunding.Common.Models.UnitTests
+{
+    [TestClass]
+    public class ExtensionModelTests
+    {
+        [TestMethod]
+        public void ToBatches()
+        {
+            IEnumerable<int> testNumbers = Enumerable.Range(0, 5026);
+
+            IEnumerable<IEnumerable<int>> results = testNumbers.ToBatches(1000);
+
+            results
+                .Should()
+                .HaveCount(6);
+
+            results
+                .First()
+               .Should()
+               .HaveCount(1000);
+
+            results
+                .Last()
+                .Should()
+                .HaveCount(26);
+        }
+    }
+}
