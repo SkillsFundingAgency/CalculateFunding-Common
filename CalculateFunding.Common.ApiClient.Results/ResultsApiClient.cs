b5fc949ef16cba5e100bb30ecd8d5b2cb04f8ae0
TATE, Matthew <Matthew.TATE@education.gov.uk>
2020-01-14 14:54:12 +0000

    Merged PR 5844: Fix to add results into the base url of the results common api client
    Fix to add results into the base url of the results common api client
    
    Related work items: #35135

--- CalculateFunding.Common.ApiClient.Results/ResultsApiClient.cs
+++ CalculateFunding.Common.ApiClient.Results/ResultsApiClient.cs
@@ -13,6 +13,8 @@ namespace CalculateFunding.Common.ApiClient.Results
 {
     public class ResultsApiClient : BaseApiClient, IResultsApiClient
     {
+        private const string UrlRoot = "results";
+
         public ResultsApiClient(IHttpClientFactory httpClientFactory, ILogger logger, ICancellationTokenProvider cancellationTokenProvider = null)
             : base(httpClientFactory, HttpClientKeys.Results, logger, cancellationTokenProvider)
         {
@@ -22,28 +24,28 @@ namespace CalculateFunding.Common.ApiClient.Results
         {
             Guard.IsNullOrWhiteSpace(providerId, nameof(providerId));
 
-            return await GetAsync<IEnumerable<string>>($"get-provider-specs?providerId={providerId}");
+            return await GetAsync<IEnumerable<string>>($"{UrlRoot}/get-provider-specs?providerId={providerId}");
         }
 
         public async Task<ApiResponse<ProviderResult>> GetProviderResults(string providerId, string specificationId)
         {
             EnsureProviderIdAndSpecificationIdSupplied(providerId, specificationId);
 
-            return await GetAsync<ProviderResult>($"get-provider-results?providerId={providerId}&specificationId={specificationId}");
+            return await GetAsync<ProviderResult>($"{UrlRoot}/get-provider-results?providerId={providerId}&specificationId={specificationId}");
         }
 
         public async Task<ApiResponse<ProviderResult>> GetProviderResultByCalculationTypeTemplate(string providerId, string specificationId)
         {
             EnsureProviderIdAndSpecificationIdSupplied(providerId, specificationId);
 
-            return await GetAsync<ProviderResult>($"specifications/{specificationId}/provider-result-by-calculationtype/{providerId}/template");
+            return await GetAsync<ProviderResult>($"{UrlRoot}/specifications/{specificationId}/provider-result-by-calculationtype/{providerId}/template");
         }
 
         public async Task<ApiResponse<ProviderResult>> GetProviderResultByCalculationTypeAdditional(string providerId, string specificationId)
         {
             EnsureProviderIdAndSpecificationIdSupplied(providerId, specificationId);
 
-            return await GetAsync<ProviderResult>($"specifications/{specificationId}/provider-result-by-calculationtype/{providerId}/additional");
+            return await GetAsync<ProviderResult>($"{UrlRoot}/specifications/{specificationId}/provider-result-by-calculationtype/{providerId}/additional");
         }
 
         public async Task<ApiResponse<IEnumerable<ProviderSourceDataset>>> GetProviderSourceDataSetsByProviderIdAndSpecificationId(string providerId, string specificationId)
@@ -51,26 +53,26 @@ namespace CalculateFunding.Common.ApiClient.Results
             EnsureProviderIdAndSpecificationIdSupplied(providerId, specificationId);
 
             return await GetAsync<IEnumerable<ProviderSourceDataset>>(
-                $"get-provider-source-datasets?providerId={providerId}&specificationId={specificationId}");
+                $"{UrlRoot}/get-provider-source-datasets?providerId={providerId}&specificationId={specificationId}");
         }
 
         public async Task<HttpStatusCode> ReIndexCalculationProviderResults()
         {
-            return await GetAsync("reindex-calc-provider-results");
+            return await GetAsync($"{UrlRoot}/reindex-calc-provider-results");
         }
 
         public async Task<ApiResponse<CalculationProviderResultSearchResults>> SearchCalculationProviderResults(SearchModel search)
         {
             Guard.ArgumentNotNull(search, nameof(search));
 
-            return await PostAsync<CalculationProviderResultSearchResults, SearchModel>($"calculation-provider-results-search", search);
+            return await PostAsync<CalculationProviderResultSearchResults, SearchModel>($"{UrlRoot}/calculation-provider-results-search", search);
         }
 
         public async Task<ApiResponse<IEnumerable<string>>> GetScopedProviderIdsBySpecificationId(string specificationId)
         {
             Guard.IsNullOrWhiteSpace(specificationId, nameof(specificationId));
 
-            return await GetAsync<IEnumerable<string>>($"get-scoped-providerids?specificationId={specificationId}");
+            return await GetAsync<IEnumerable<string>>($"{UrlRoot}/get-scoped-providerids?specificationId={specificationId}");
         }
 
         public async Task<ApiResponse<IEnumerable<FundingCalculationResultsTotals>>> GetFundingCalculationResultsForSpecifications(SpecificationListModel specificationList)
@@ -78,28 +80,28 @@ namespace CalculateFunding.Common.ApiClient.Results
             Guard.ArgumentNotNull(specificationList, nameof(specificationList));
 
             return await PostAsync<IEnumerable<FundingCalculationResultsTotals>, SpecificationListModel>(
-                $"get-calculation-result-totals-for-specifications", specificationList);
+                $"{UrlRoot}/get-calculation-result-totals-for-specifications", specificationList);
         }
 
         public async Task<ApiResponse<IEnumerable<ProviderResult>>> GetProviderResultsBySpecificationId(string specificationId, string top = null)
         {
             Guard.IsNullOrWhiteSpace(specificationId, nameof(specificationId));
 
-            return await GetAsync<IEnumerable<ProviderResult>>($"get-specification-provider-results?specificationId={specificationId}&top={top}");
+            return await GetAsync<IEnumerable<ProviderResult>>($"{UrlRoot}/get-specification-provider-results?specificationId={specificationId}&top={top}");
         }
 
         public async Task<ApiResponse<bool>> HasCalculationResults(string calculationId)
         {
             Guard.IsNullOrWhiteSpace(calculationId, nameof(calculationId));
 
-            return await GetAsync<bool>($"hasCalculationResults/{calculationId}");
+            return await GetAsync<bool>($"{UrlRoot}/hasCalculationResults/{calculationId}");
         }
 
         public async Task<ApiResponse<IEnumerable<string>>> GetSpecificationIdsForProvider(string providerId)
         {
             Guard.IsNullOrWhiteSpace(providerId, nameof(providerId));
 
-            return await GetAsync<IEnumerable<string>>($"get-provider-specs?providerId={providerId}");
+            return await GetAsync<IEnumerable<string>>($"{UrlRoot}/get-provider-specs?providerId={providerId}");
         }
 
         private void EnsureProviderIdAndSpecificationIdSupplied(string providerId, string specificationId)
