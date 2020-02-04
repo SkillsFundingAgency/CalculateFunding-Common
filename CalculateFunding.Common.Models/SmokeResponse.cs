3b7620c9935b59bf7f20279fdd67a4f6ccfc2266
TATE, Matthew <Matthew.TATE@education.gov.uk>
2020-02-04 10:05:33 +0000

    Merged PR 6032: Adde smoke response model to common
    Adde smoke response model to common
    
    Related work items: #35510

---
+++ CalculateFunding.Common.Models/SmokeResponse.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CalculateFunding.Common.Models
+{
+    public class SmokeResponse
+    {
+        [JsonProperty("listener")]
+        public string Listener { get; set; }
+
+        [JsonProperty("invocationId")]
+        public string InvocationId { get; set; }
+
+        [JsonProperty("service")]
+        public string Service { get; set; }
+    }
+}
