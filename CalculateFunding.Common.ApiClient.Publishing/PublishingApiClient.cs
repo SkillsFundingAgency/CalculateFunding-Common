b68182e30daf0b38745455ceab54126a9ee84d1c
TATE, Matthew <Matthew.TATE@education.gov.uk>
2020-02-10 11:16:20 +0000

    Merged PR 6084: Add method call in publishing client to retrieve funding approval transactions
    Add method call in publishing client to retrieve funding approval transactions for a provider within a specification
    
    Related work items: #36090

--- CalculateFunding.Common.ApiClient.Publishing/PublishingApiClient.cs
+++ CalculateFunding.Common.ApiClient.Publishing/PublishingApiClient.cs
@@ -28,6 +28,16 @@ namespace CalculateFunding.Common.ApiClient.Publishing
             return await GetAsync<PublishedProviderVersion>(url);
         }
 
+        public async Task<ApiResponse<IEnumerable<PublishedProviderTransaction>>> GetPublishedProviderTransactions(string specificationId, string providerId)
+        {
+            Guard.IsNullOrWhiteSpace(specificationId, nameof(specificationId));
+            Guard.IsNullOrWhiteSpace(providerId, nameof(providerId));
+
+            string url = $"publishedprovidertransactions/{specificationId}/{providerId}";
+
+            return await GetAsync<IEnumerable<PublishedProviderTransaction>>(url);
+        }
+
         public async Task<ApiResponse<string>> GetPublishedProviderVersionBody(string publishedProviderVersionId)
         {
             Guard.IsNullOrWhiteSpace(publishedProviderVersionId, nameof(publishedProviderVersionId));
