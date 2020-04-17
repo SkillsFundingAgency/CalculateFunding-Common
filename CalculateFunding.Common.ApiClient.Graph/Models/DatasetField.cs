8e40fa516bdf6a969f1c25cd8fb064ea3811f083
Rohit AMBLEKAR <Rohit.AMBLEKAR@education.gov.uk>
2020-04-17 07:43:05 +0000

    Merged PR 6807: Fixed typo mistake and added few properties to Model
    Fixed typo mistake and added few properties to Model
    
    Related work items: #40701

--- CalculateFunding.Common.ApiClient.Graph/Models/DatasetField.cs
+++ CalculateFunding.Common.ApiClient.Graph/Models/DatasetField.cs
@@ -1,29 +1,33 @@
 ﻿using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace CalculateFunding.Common.ApiClient.Graph.Models
 {
     public class DatasetField
     {
 
-        [JsonProperty("datasetfielrelatioshipdname")]
-        public string DatasetFielRelatioshipdName { get; set; }
+        [JsonProperty("datasetfieldrelatioshipname")]
+        public string DatasetFieldRelatioshipName { get; set; }
         [JsonProperty("specificationid")]
         public string SpecificationId { get; set; }
+
+        [JsonProperty("calculationid")]
+        public string CalculationId { get; set; }
+
+        [JsonProperty("propertyname")]
+        public string PropertyName { get; set; }
+
         [JsonProperty("datasetrelationshipid")]
         public string DatasetRelationshipId { get; set; }
         [JsonProperty("schemaid")]
         public string SchemaId { get; set; }
-        [JsonProperty("schemaFieldid")] 
+        [JsonProperty("schemaFieldid")]
         public string SchemaFieldId { get; set; }
         [JsonProperty("datasetfieldname")]
         public string DatasetFieldName { get; set; }
         [JsonProperty("datasetfieldid")]
         public string DatasetFieldId { get; set; }
         [JsonProperty("datasetfieldisaggregable")]
-        public bool DatasetFieldIsAggregable { get; set; }       
+        public bool DatasetFieldIsAggregable { get; set; }
 
     }
 }
