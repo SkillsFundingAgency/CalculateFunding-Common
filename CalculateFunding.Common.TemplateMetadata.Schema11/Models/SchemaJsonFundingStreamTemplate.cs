e57498fb99b12d88bc4a05ce0b1ffabf79829559
Pete Moody <pete.moody@education.gov.uk>
2020-06-01 16:52:28 +0000

    Merged PR 7406: remove fields that have been removed from latest 1.1 schema
    remove fields that have been removed from latest 1.1 schema
    
    Related work items: #46377

--- CalculateFunding.Common.TemplateMetadata.Schema11/Models/SchemaJsonFundingStreamTemplate.cs
+++ CalculateFunding.Common.TemplateMetadata.Schema11/Models/SchemaJsonFundingStreamTemplate.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
 namespace CalculateFunding.Common.TemplateMetadata.Schema11.Models
 {
     public class SchemaJsonFundingStreamTemplate
     {
-        public string TemplateStatus { get; set; }
-        public DateTime TemplateStatusChangedDate { get; set; }
         public string FundingTemplateVersion { get; set; }
         public SchemaJsonFundingStream FundingStream { get; set; }
         public SchemaJsonFundingPeriod FundingPeriod { get; set; }
