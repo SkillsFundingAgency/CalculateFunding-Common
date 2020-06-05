26911c61e54da9f6085ae61f93104ffad4812f58
Pete Moody <pete.moody@education.gov.uk>
2020-06-05 12:16:12 +0000

    Merged PR 7492: updates to support latest changes to schema 1.1
    updates to support latest changes to schema 1.1
    
    Related work items: #46670, #46820

--- CalculateFunding.Common.TemplateMetadata.Schema11/Models/SchemaJsonFundingStreamTemplate.cs
+++ CalculateFunding.Common.TemplateMetadata.Schema11/Models/SchemaJsonFundingStreamTemplate.cs
@@ -4,9 +4,6 @@ namespace CalculateFunding.Common.TemplateMetadata.Schema11.Models
 {
     public class SchemaJsonFundingStreamTemplate
     {
-        public string FundingTemplateVersion { get; set; }
-        public SchemaJsonFundingStream FundingStream { get; set; }
-        public SchemaJsonFundingPeriod FundingPeriod { get; set; }
         public IEnumerable<SchemaJsonFundingLine> FundingLines { get; set; }
     }
 }
