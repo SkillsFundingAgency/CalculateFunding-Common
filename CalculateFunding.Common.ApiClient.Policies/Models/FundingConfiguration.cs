064c7290ed4df2f255da2268be87d49a530f38ec
TATE, Matthew <Matthew.TATE@education.gov.uk>
2021-01-08 16:09:31 +0000

    Merged PR 10241: Added error detectors to funding configuration model.
    Added error detectors to funding configuration model.
    
    Related work items: #57437

--- CalculateFunding.Common.ApiClient.Policies/Models/FundingConfiguration.cs
+++ CalculateFunding.Common.ApiClient.Policies/Models/FundingConfiguration.cs
@@ -26,6 +26,9 @@ namespace CalculateFunding.Common.ApiClient.Policies.Models.FundingConfig
         [JsonProperty("variations")]
         public IEnumerable<FundingVariation> Variations { get; set; }
 
+        [JsonProperty("errorDetectors")]
+        public IEnumerable<string> ErrorDetectors { get; set; }
+
         [JsonProperty("approvalMode")]
         public ApprovalMode ApprovalMode { get; set; }
 
@@ -34,7 +37,6 @@ namespace CalculateFunding.Common.ApiClient.Policies.Models.FundingConfig
 
         [JsonProperty("paymentOrganisationSource")]
         public PaymentOrganisationSource PaymentOrganisationSource { get; set; }
-
 
         [JsonProperty("updateCoreProviderVersion")]
         public UpdateCoreProviderVersion UpdateCoreProviderVersion { get; set; }
