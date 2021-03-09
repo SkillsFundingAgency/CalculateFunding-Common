94b0a75e93fea6bd015ac76fe45fd9650bbf3fe2
TATE, Matthew <Matthew.TATE@education.gov.uk>
2021-03-09 11:05:41 +0000

    Merged PR 11044: Changed the id of the enum to be unique by specification and funding stream.
    Changed the id of the enum to be unique by specification and funding stream.
    
    Related work items: #61152

--- CalculateFunding.Common.ApiClient.Graph/Models/Enum.cs
+++ CalculateFunding.Common.ApiClient.Graph/Models/Enum.cs
@@ -9,7 +9,16 @@ namespace CalculateFunding.Common.ApiClient.Graph.Models
         public const string IdField = "enumid";
 
         [JsonProperty(IdField)]
-        public string EnumId => $"{EnumName}.{EnumValue}";
+        public string EnumId => $"{SpecificationId}-{FundingStreamId}-{EnumName}-{EnumValue}";
+        
+        [JsonProperty("specificationid")]
+        public string SpecificationId { get; set; }
+
+        [JsonProperty("fundingstreamid")]
+        public string FundingStreamId { get; set; }
+
+        [JsonProperty("codereference")]
+        public string CodeReference => $"{EnumName}.{EnumValue}";
 
         [JsonProperty("enumname")]
         public string EnumName { get; set; }
