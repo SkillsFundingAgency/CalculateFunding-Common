bdf11e88684f4bd2f7fc6fa7ce0024135decfa46
Dan Symonds-Lloyd <Daniel.SYMONDS-LLOYD@education.gov.uk>
2021-06-28 12:19:58 +0000

    Merged PR 11444: Updated funding line profile for screen fixups
    Updated funding line profile for screen fixups
    
    Related work items: #62043

--- CalculateFunding.Common.ApiClient.Publishing/Models/FundingLineProfile.cs
+++ CalculateFunding.Common.ApiClient.Publishing/Models/FundingLineProfile.cs
@@ -6,23 +6,93 @@ namespace CalculateFunding.Common.ApiClient.Publishing.Models
 {
     public class FundingLineProfile
     {
+        /// <summary>
+        /// Funding line code
+        /// </summary>
         public string FundingLineCode { get; set; }
+
+        /// <summary>
+        /// Funding line display name
+        /// </summary>
         public string FundingLineName { get; set; }
-        public decimal? TotalAllocation { get; set; }
+
+        /// <summary>
+        /// Total funding line amount - from calculations
+        /// </summary>
+        public decimal? FundingLineAmount { get; set; }
+
+        /// <summary>
+        /// Total funding allocated to profile patterns - may be different to the funding line amount when custom profiles are set
+        /// </summary>
+        public decimal? ProfilePatternTotal { get; set; }
+
+        /// <summary>
+        /// Total amount already paid during this period
+        /// </summary>
         public decimal AmountAlreadyPaid { get; set; }
+
+        /// <summary>
+        /// The amount which is unpaid in this period
+        /// </summary>
         public decimal? RemainingAmount { get; set; }
+
+        /// <summary>
+        /// Carry over amount to next funding period
+        /// </summary>
         public decimal? CarryOverAmount { get; set; }
+
+        /// <summary>
+        /// The total profile values with carry over included. May be different to Funding Line Total
+        /// </summary>
+        public decimal? ProfilePatternTotalWithCarryOver { get; set; }
+
+        /// <summary>
+        /// Provider ID
+        /// </summary>
         public string ProviderId { get; set; }
+
+        /// <summary>
+        /// Provider name
+        /// </summary>
         public string ProviderName { get; set; }
+
+        /// <summary>
+        /// UKPRN of the provider
+        /// </summary>
         public string UKPRN { get; set; }
+
+        /// <summary>
+        /// Profiling service pattern key
+        /// </summary>
         public string ProfilePatternKey { get; set; }
+
+        /// <summary>
+        /// Profile pattern display name
+        /// </summary>
         public string ProfilePatternName { get; set; }
+
+        /// <summary>
+        /// Profile pattern description
+        /// </summary>
         public string ProfilePatternDescription { get; set; }
+
+        /// <summary>
+        /// Is this provider configured with a custom profile
+        /// </summary>
         public bool IsCustomProfile { get; set; }
+
         public Reference LastUpdatedUser { get; set; }
-        public DateTime? LastUpdatedDate { get; set; }
-        public decimal? ProfileTotalAmount { get; set; }
+
+        public DateTimeOffset? LastUpdatedDate { get; set; }
+
+        /// <summary>
+        /// Profile totals
+        /// </summary>
         public IEnumerable<ProfileTotal> ProfileTotals { get; set; }
+
+        /// <summary>
+        /// Funding line errors
+        /// </summary>
         public IEnumerable<PublishedProviderError> Errors { get; set; }
     }
 }
