a9735243cd0eb88ef2acc18802391f118310ad3d
Nikhil DINESH <Nikhil.DINESH@education.gov.uk>
2023-06-08 16:03:24 +0000

    Merged PR 22152: PR:127132 Create dataset page changes - Common Unit test project
    PR:127132 Create dataset page changes - Common Unit test project
    
    Related work items: #127132

---
+++ calculatefunding.common.apiclient.fds.UnitTests/FDSApiClientTests.cs.cs
@@ -0,0 +1,49 @@
+﻿using System.Threading.Tasks;
+using CalculateFunding.Common.Testing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
+using Serilog;
+using CalculateFunding.Common.ApiClient.FDS;
+using CalculateFunding.Common.ApiClient.FDS.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+// ReSharper disable HeapView.CanAvoidClosure
+
+namespace CalculateFunding.Common.ApiClient.Datasets.UnitTests
+{
+    [TestClass]
+    public class DatasetsApiClientTests : ApiClientTestBase
+    {
+        private FDSApiClient _client;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            _client = new FDSApiClient(ClientFactory,
+                Substitute.For<ILogger>());
+        }
+
+        [TestMethod]
+        public async Task GetDatasetDefinitionsMakesGetCall()
+        {
+            string id = NewRandomString();
+
+            await AssertGetRequest($"FundingData/schema/{id}",
+                id,
+                new FDSDatasetDefinition(),
+                _ => _client.GetDatasetDefinition(_));
+        }
+
+        [TestMethod]
+        public async Task GetDatasetForFundingStreamMakesGetCall()
+        {
+            string fundingStream = NewRandomString();
+
+            await AssertGetRequest($"FundingData/schemas/{fundingStream}",
+                fundingStream,
+                Enumerable.Empty< DatasetDefinitionByFundingStream >(),
+                _ => _client.GetDatasetForFundingStream(_));
+        }
+    }
+}
