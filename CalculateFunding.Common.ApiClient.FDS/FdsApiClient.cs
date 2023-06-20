6747f06e96a959b2a0d762e6b44734aff0b12b10
Nikhil DINESH <Nikhil.DINESH@education.gov.uk>
2023-06-20 15:55:15 +0000

    Merged PR 22309: PR:132035 In 1619 stream, fields under different Schemas are getting picked f...
    PR:132035 In 1619 stream, fields under different Schemas are getting picked from CFS Schema and not FDS schema - Fixed
    
    Related work items: #132035

--- CalculateFunding.Common.ApiClient.FDS/FdsApiClient.cs
+++ CalculateFunding.Common.ApiClient.FDS/FdsApiClient.cs
@@ -7,11 +7,13 @@ using System.Threading;
 using System;
 using System.Collections.Generic;
 using CalculateFunding.Common.ApiClient.FDS.Models;
+using System.Linq;
 
 namespace CalculateFunding.Common.ApiClient.FDS
 {
     public class FDSApiClient : BaseApiClient, IFDSApiClient
     {
+        private const string FDSPrefix = "FDS_";
         public FDSApiClient(IHttpClientFactory httpClientFactory, ILogger logger, ICancellationTokenProvider cancellationTokenProvider = null) 
             : base(httpClientFactory, HttpClientKeys.FDS, logger, cancellationTokenProvider)
         {
@@ -21,11 +23,21 @@ namespace CalculateFunding.Common.ApiClient.FDS
         {
             return await GetAsync<IEnumerable<DatasetDefinitionByFundingStream>>($"FundingData/schemas/" + fundingStream);
         }
-
         public async Task<ApiResponse<FDSDatasetDefinition>> GetDatasetDefinition(string definitionId)
         {
-            return await GetAsync<FDSDatasetDefinition>($"FundingData/schema/" + definitionId);
+            ApiResponse<FDSDatasetDefinition> fdsDefinition = await GetAsync<FDSDatasetDefinition>($"FundingData/schema/" + definitionId);
+            if (fdsDefinition == null || fdsDefinition.Content == null)
+            {
+                return fdsDefinition;
+            }
+            if (fdsDefinition.Content.Name != null)
+            {
+                fdsDefinition.Content.Name = FDSPrefix + fdsDefinition.Content.Name;
+                fdsDefinition.Content.FDSTableDefinitions[0].Name = fdsDefinition.Content.Name;
+            }
+            return fdsDefinition;
         }
+
         public async Task<ApiResponse<IEnumerable<FDSDatasetVersion>>> GetDatasetVersionsByDefinitionId(string definitionId)
         {
             return await GetAsync<IEnumerable<FDSDatasetVersion>>($"FundingData/FundingDataVersions/Schema/" + definitionId);
