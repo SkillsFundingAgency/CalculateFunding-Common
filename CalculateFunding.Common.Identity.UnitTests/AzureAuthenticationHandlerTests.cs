036eb9ca56c5ecf518c9ceed5993e1974312c505
Akhil.NUTHAKKI <Akhil.NUTHAKKI@EDUCATION.GOV.UK>
2025-02-14 12:49:00 +0000

    Resolved Unit Test case failure

--- CalculateFunding.Common.Identity.UnitTests/AzureAuthenticationHandlerTests.cs
+++ CalculateFunding.Common.Identity.UnitTests/AzureAuthenticationHandlerTests.cs
@@ -68,6 +68,7 @@ namespace CalculateFunding.Common.Identity.UnitTests
 
             ILoggerFactory logger = Substitute.For<ILoggerFactory>();
             UrlEncoder encoder = Substitute.For<UrlEncoder>();
+            ISystemClock clock = new SystemClock();
 
             HeaderDictionary headers = new HeaderDictionary
             {
@@ -82,7 +83,7 @@ namespace CalculateFunding.Common.Identity.UnitTests
 
             AuthenticationScheme scheme = new AuthenticationScheme(AzureAuthenticationDefaults.AuthenticationScheme, AzureAuthenticationDefaults.DisplayName, typeof(AzureAuthenticationHandler));
 
-            AzureAuthenticationHandler handler = new AzureAuthenticationHandler(optionsMonitor, logger, encoder);
+            AzureAuthenticationHandler handler = new AzureAuthenticationHandler(optionsMonitor, logger, encoder, clock);
             await handler.InitializeAsync(scheme, context);
             return handler;
         }
